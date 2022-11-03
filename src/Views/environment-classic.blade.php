dc515f2b86aad06e4f4ddcebb39176720179ae16
Mottasin Leemon <105141294+leemonCodeShaper@users.noreply.github.com>
2022-11-03 13:54:47 +0600

    fix: first save env and then save & exit condition added

--- src/Views/environment-classic.blade.php
+++ src/Views/environment-classic.blade.php
@@ -9,24 +9,21 @@
 @endsection
 
 @section('container')
-
     <form method="post" action="{{ route('LaravelInstaller::environmentSaveClassic') }}">
         {!! csrf_field() !!}
         <textarea class="textarea" name="envConfig">{{ $envConfig }}</textarea>
-        <div class="buttons buttons--right">
-            <button class="button button--light" type="submit">
-            	<i class="fa fa-floppy-o fa-fw" aria-hidden="true"></i>
-             	{!! trans('installer_messages.environment.classic.save') !!}
-            </button>
-        </div>
+        @if (!Session::has('message'))
+            <div class="buttons buttons--right">
+                <button class="button button--light" type="submit">
+                    <i class="fa fa-floppy-o fa-fw" aria-hidden="true"></i>
+                    {!! trans('installer_messages.environment.classic.save') !!}
+                </button>
+            </div>
+        @endif
     </form>
 
-    @if( ! isset($environment['errors']))
+    @if (!isset($environment['errors']) && Session::has('message'))
         <div class="buttons-container">
-            <a class="button float-left" href="{{ route('LaravelInstaller::environmentWizard') }}">
-                <i class="fa fa-sliders fa-fw" aria-hidden="true"></i>
-                {!! trans('installer_messages.environment.classic.back') !!}
-            </a>
             <a class="button float-right" href="{{ route('LaravelInstaller::database') }}">
                 <i class="fa fa-check fa-fw" aria-hidden="true"></i>
                 {!! trans('installer_messages.environment.classic.install') !!}
@@ -34,5 +31,4 @@
             </a>
         </div>
     @endif
-
 @endsection
